3993d7d3e47147d646792d967ca9733558e83d7c
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-10-23 18:08:28 +0100

    Start work selecting a puzzle

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,21 +1,26 @@
 ﻿using MetroLog.Maui;
 using Microsoft.Extensions.Logging;
+using System.Windows.Input;
 
 namespace SudokuDlxMaui;
 
 public partial class MainPage : ContentPage
 {
   private ILogger<MainPage> _logger;
+  private LogController _logController;
   private Puzzle _currentPuzzle;
 
   public MainPage(ILogger<MainPage> logger)
   {
-    _currentPuzzle = SamplePuzzles.Puzzles[3];
+    _logger = logger;
+    _logController = new LogController();
+    _currentPuzzle = SamplePuzzles.Puzzles[0];
     InitializeComponent();
     SudokuPuzzleGraphicsView.Drawable = new SudokuPuzzleDrawable(this);
-    BindingContext = new LogController();
-    _logger = logger;
+    BindingContext = this;
   }
+
+  public ICommand GoToLogsPageCommand { get => _logController.GoToLogsPageCommand; }
 
   public void OnSizeChanged(object sender, EventArgs e)
   {
@@ -23,4 +28,6 @@ public partial class MainPage : ContentPage
   }
 
   public Puzzle CurrentPuzzle { get => _currentPuzzle; }
+  public Puzzle[] Puzzles { get => SamplePuzzles.Puzzles; }
+  public int SelectedPuzzleIndex { get => 0; }
 }
