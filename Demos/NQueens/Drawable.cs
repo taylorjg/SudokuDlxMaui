9bea3cb584c2617682f8d45e552085335e49739b
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-11-09 00:17:41 +0000

    Basic N Queens drawable

--- Demos/NQueens/Drawable.cs
+++ Demos/NQueens/Drawable.cs
@@ -3,6 +3,10 @@ namespace SudokuDlxMaui.Demos.NQueens;
 public class NQueensDrawable : IDrawable
 {
   private DemoPageViewModel _demoPageViewModel;
+  private float _squareWidth;
+  private float _squareHeight;
+
+  private const int N = 8;
 
   public NQueensDrawable(DemoPageViewModel demoPageViewModel)
   {
@@ -11,5 +15,42 @@ public class NQueensDrawable : IDrawable
 
   public void Draw(ICanvas canvas, RectF dirtyRect)
   {
+    _squareWidth = dirtyRect.Width / N;
+    _squareHeight = dirtyRect.Height / N;
+    DrawGrid(canvas);
+    var internalRows = _demoPageViewModel.SolutionInternalRows.Cast<NQueensInternalRow>();
+    foreach (var internalRow in internalRows)
+    {
+      var row = internalRow.Coords.Row;
+      var col = internalRow.Coords.Col;
+      DrawSquare(canvas, row, col, Colors.Red);
+    }
+  }
+
+  private void DrawGrid(ICanvas canvas)
+  {
+    var locations =
+      Enumerable.Range(0, N).SelectMany(row =>
+        Enumerable.Range(0, N).Select(col =>
+          new Coords(row, col))).ToArray();
+
+    foreach (var coords in locations)
+    {
+      var row = coords.Row;
+      var col = coords.Col;
+      var colour = (row + col) % 2 == 0 ? Colors.Black : Colors.White;
+      DrawSquare(canvas, row, col, colour);
+    }
+  }
+
+  private void DrawSquare(ICanvas canvas, int row, int col, Color colour)
+  {
+    var x = _squareWidth * col;
+    var y = _squareHeight * row;
+    var width = _squareWidth;
+    var height = _squareHeight;
+
+    canvas.FillColor = colour;
+    canvas.FillRectangle(x, y, width, height);
   }
 }
